2fdb1fc1b405064448fdcbb7f67ecf266e4bf524
gabrielalb5 <gabrielalbino28@gmail.com>
2026-08-16 20:03:33 -0300

    primeiro commit

---
+++ src/main/java/br/org/larescolaredencao/LarescolaredencaoApplication.java
@@ -0,0 +1,13 @@
+package br.org.larescolaredencao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LarescolaredencaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LarescolaredencaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/org/larescolaredencao/LarescolaredencaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.org.larescolaredencao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LarescolaredencaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
