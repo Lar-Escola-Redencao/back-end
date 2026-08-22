a6c11bb53edd005e26339ecb1bd5c67cc823a557
eduboni <eduboni10@gmail.com>
2026-08-22 11:41:27 -0300

    feat: cors security config

--- src/main/java/br/org/larescolaredencao/security/SecurityConfig.java
+++ src/main/java/br/org/larescolaredencao/security/SecurityConfig.java
@@ -6,12 +6,17 @@ posteriormente, será necessário atualizar esse arquivo bloqueando as rotas de 
 
 package br.org.larescolaredencao.security;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -19,15 +24,25 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-            // Desabilita a proteção CSRF, necessária para testarmos APIs REST no Postman
-            .csrf(AbstractHttpConfigurer::disable)
-            // Configura a autorização das requisições
-            .authorizeHttpRequests(auth -> auth
-                // Libera temporariamente o acesso a todas as rotas
-                .anyRequest().permitAll() 
-            );
+        return http
+        		.cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                )
+                .build();
 
-        return http.build();
+    }
+    
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
+        
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
